24267ad441a1b4a9c9da3a273e63deeb2dfb36b8
Gregory Vance <13666175+gsvance@users.noreply.github.com>
2026-02-01 15:55:36 -0500

    Add type hints to trig functions

--- degree_math.py
+++ degree_math.py
@@ -6,34 +6,37 @@ the trig functions are in degree mode instead of defaulting to radians.
 """
 
 import math as _math
+import typing as _typing
 
 from math import *
 
 
-def sin(x):
+type _SupportsFloatOrIndex = _typing.SupportsFloat | _typing.SupportsIndex
+
+
+def sin(x: _SupportsFloatOrIndex, /) -> float:
     return _math.sin(_math.degrees(x))
 
 
-def cos(x):
+def cos(x: _SupportsFloatOrIndex, /) -> float:
     return _math.cos(_math.degrees(x))
 
 
-def tan(x):
+def tan(x: _SupportsFloatOrIndex, /) -> float:
     return _math.tan(_math.degrees(x))
 
 
-def asin(x):
+def asin(x: _SupportsFloatOrIndex, /) -> float:
     return _math.degrees(_math.asin(x))
 
 
-def acos(x):
+def acos(x: _SupportsFloatOrIndex, /) -> float:
     return _math.degrees(_math.acos(x))
 
 
-def atan(x):
+def atan(x: _SupportsFloatOrIndex) -> float:
     return _math.degrees(_math.atan(x))
 
 
-def atan2(y, x):
+def atan2(y: _SupportsFloatOrIndex, x: _SupportsFloatOrIndex, /) -> float:
     return _math.degrees(_math.atan2(y, x))
-
