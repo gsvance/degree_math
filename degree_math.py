4ec4e03ef60424114e11e9033a4dd3899adfd20f
Gregory Vance <13666175+gsvance@users.noreply.github.com>
2026-02-01 16:22:30 -0500

    Add a decorator to rewrite trig function docstrings

--- degree_math.py
+++ degree_math.py
@@ -14,29 +14,56 @@ from math import *
 type _SupportsFloatOrIndex = _typing.SupportsFloat | _typing.SupportsIndex
 
 
+# This decorator writes docstrings for the trig functions in this module by
+# looking up the corresponding function in the built-in math module and
+# replacing a few select substrings in its docstring
+def _rewrite_docstring[**P, R](
+    new_function: _typing.Callable[P, R],
+) -> _typing.Callable[P, R]:
+
+    old_function = getattr(_math, new_function.__name__)
+    assert isinstance(old_function.__doc__, str)
+
+    new_function.__doc__ = (
+        old_function.__doc__
+        .replace('radians', 'degrees')
+        .replace('pi/2', '90')
+        .replace('pi', '180')
+    )
+
+    return new_function
+
+
+@_rewrite_docstring
 def sin(x: _SupportsFloatOrIndex, /) -> float:
     return _math.sin(_math.degrees(x))
 
 
+@_rewrite_docstring
 def cos(x: _SupportsFloatOrIndex, /) -> float:
     return _math.cos(_math.degrees(x))
 
 
+@_rewrite_docstring
 def tan(x: _SupportsFloatOrIndex, /) -> float:
     return _math.tan(_math.degrees(x))
 
 
+@_rewrite_docstring
 def asin(x: _SupportsFloatOrIndex, /) -> float:
     return _math.degrees(_math.asin(x))
 
 
+@_rewrite_docstring
 def acos(x: _SupportsFloatOrIndex, /) -> float:
     return _math.degrees(_math.acos(x))
 
 
+@_rewrite_docstring
 def atan(x: _SupportsFloatOrIndex) -> float:
     return _math.degrees(_math.atan(x))
 
 
+@_rewrite_docstring
 def atan2(y: _SupportsFloatOrIndex, x: _SupportsFloatOrIndex, /) -> float:
     return _math.degrees(_math.atan2(y, x))
