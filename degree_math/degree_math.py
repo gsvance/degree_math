1e39fbe40efff1429a3a8058904b45d3d5d04aaa
Gregory Vance <13666175+gsvance@users.noreply.github.com>
2026-02-28 14:27:18 -0500

    Create initial implementation of the mode switch

--- degree_math/degree_math.py
+++ degree_math/degree_math.py
@@ -19,6 +19,37 @@ from math import *
 type _SupportsFloatOrIndex = _typing.SupportsFloat | _typing.SupportsIndex
 
 
+type _Converter = _typing.Callable[[_SupportsFloatOrIndex], float]
+
+_forward_converter: _Converter = _math.radians
+_inverse_converter: _Converter = _math.degrees
+
+
+def set_mode(
+    mode: _typing.Literal['degrees', 'radians'] | None = None,
+) -> _typing.Literal['degrees', 'radians']:
+    """Set the trig function mode to 'degrees' or 'radians'.
+
+    Returns the current mode as a string, even if called without arguments.
+    """
+    global _forward_converter, _inverse_converter
+    match mode:
+        case 'degrees':
+            _forward_converter = _math.radians
+            _inverse_converter = _math.degrees
+            return mode
+        case 'radians':
+            _forward_converter = float
+            _inverse_converter = float
+            return mode
+        case None:
+            if _forward_converter is _math.radians:
+                return 'degrees'
+            return 'radians'
+        case _:
+            raise TypeError(f'value for mode cannot be {mode!r}')
+
+
 # The following decorator writes the docstrings for the trig functions in this
 # module by looking up the corresponding functions in the standard math module
 # and then replacing a few select substrings.
@@ -45,17 +76,17 @@ def _rewrite_docstring[**P, R](
 
 @_rewrite_docstring
 def sin(x: _SupportsFloatOrIndex, /) -> float:
-    return _math.sin(_math.radians(x))
+    return _math.sin(_forward_converter(x))
 
 
 @_rewrite_docstring
 def cos(x: _SupportsFloatOrIndex, /) -> float:
-    return _math.cos(_math.radians(x))
+    return _math.cos(_forward_converter(x))
 
 
 @_rewrite_docstring
 def tan(x: _SupportsFloatOrIndex, /) -> float:
-    return _math.tan(_math.radians(x))
+    return _math.tan(_forward_converter(x))
 
 
 # Overwrite the asin(), acos(), atan(), and atan2() functions to return values
@@ -64,19 +95,19 @@ def tan(x: _SupportsFloatOrIndex, /) -> float:
 
 @_rewrite_docstring
 def asin(x: _SupportsFloatOrIndex, /) -> float:
-    return _math.degrees(_math.asin(x))
+    return _inverse_converter(_math.asin(x))
 
 
 @_rewrite_docstring
 def acos(x: _SupportsFloatOrIndex, /) -> float:
-    return _math.degrees(_math.acos(x))
+    return _inverse_converter(_math.acos(x))
 
 
 @_rewrite_docstring
 def atan(x: _SupportsFloatOrIndex) -> float:
-    return _math.degrees(_math.atan(x))
+    return _inverse_converter(_math.atan(x))
 
 
 @_rewrite_docstring
 def atan2(y: _SupportsFloatOrIndex, x: _SupportsFloatOrIndex, /) -> float:
-    return _math.degrees(_math.atan2(y, x))
+    return _inverse_converter(_math.atan2(y, x))
